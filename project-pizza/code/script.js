a3fb0489556794538377a271d2c8036bbde8bddc
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-08-22 12:17:23 +0200

    -m new blocks

--- project-pizza/code/script.js
+++ project-pizza/code/script.js
@@ -15,29 +15,47 @@ alert (
 // Step 2 - Food choice
 // Your code goes here
 let Food = prompt ("Please enter your choice of food. Enter a number 1.Pizza 2.Pasta 3.Salad")
-if (1) { 
-  console.log ("1")
+
+//If the choice is 1, it is pizza
+//If the choice is 2, it is Pasta
+//If the choice is 3, it is Salad
+if (Food ===1) {
+ console.log ('Pizza')
 }
-if (2) {
-  console.log ("2")
+else if (Food ===2) {
+  console.log ('Pasta')
 }
-if (3) {
-  console.log ("3")
-}
-if (null) {
-  console.log ("Please enter a number")
+else if (Food ===3) {
+  console.log ('Salad')
 }
 alert (
   `You chosen ${Food}`
 )
+const validateOrderName = (orderName) => {
+  if (Food !== Pizza && Food !== 2 && Food !== 3) {
+  alert("Select a number from the menu")
+  orderName()
+} else {
+  return true
+}
+}
+
+
 // Step 3 - Subtype choice
 // Your code goes here
 let subtype  = prompt ("What kind do you want?")
 
+let confirmation = prompt ("Are you sure?")
 
 // Step 4 - Age
 // Your code goes here
-let age 
+let age = prompt ('Type your age in numbers')
+
+alert (
+  'You ordered ${Food} ${Subtype} 
+)
+
+let confirmation = prompt ("Place your order")
 
 // Step 5 - Order confirmation
 // Your code goes here
