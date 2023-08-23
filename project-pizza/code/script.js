9059bedec09f207abbdd864dc1e56a973a856d30
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-08-23 09:54:52 +0200

    -m new code

--- project-pizza/code/script.js
+++ project-pizza/code/script.js
@@ -6,7 +6,7 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
-let person = prompt ("Please enter your name")
+const person = prompt ("Please enter your name")
 
 alert (
   `Welcome ${person}`
@@ -14,28 +14,71 @@ alert (
 
 // Step 2 - Food choice
 // Your code goes here
-let Food = prompt ("Please enter your choice of food. Enter a number 1.Pizza 2.Pasta 3.Salad")
+const Food = prompt ("What's your craving today:
+1 - Pizza 
+2 - Pasta 
+3- Salad
+Please enter the number of your choice!")
+
+let selectedFood ="";
+
+
 
 //If the choice is 1, it is pizza
 //If the choice is 2, it is Pasta
 //If the choice is 3, it is Salad
 if (Food ===1) {
- console.log ('Pizza')
+ selectedFood ="Pizza";
 }
 else if (Food ===2) {
-  console.log ('Pasta')
+  selectedFood ="Pasta";
 }
 else if (Food ===3) {
-  console.log ('Salad')
+  selectedFood ="Salad";
+}
+else {
+  alert ("Invalid choice. Please select a valid number.");
+  ProcessingInstruction.exit(1);
 }
 alert (
   `You chosen ${Food}`
 )
-vnbn
+
 
 // Step 3 - Subtype choice
-// Your code goes here
-let subtype  = prompt ("What kind do you want?")
+// Your code goes here Ask the user to inser subtype based on choice of food.
+let subtype  = "";
+let subtypeName = ""; //Store name for the subtypes pizza, pasta and salad
+
+switch (Food) {
+  case "Pizza":
+    subtype = prompt ('Select the type of pizza you want:
+    1 - Margherita
+    2 - Veggie
+    3 - Nutella
+    Please enter the number of your choice:');
+    subtypeName = ["Margherita", "Veggie", "Nutella"];
+    break;
+    case "Pasta":
+      subtype = prompt ('Select the type of pizza you want:
+      1 - Shrimp
+      2 - Veggie
+      3 - Elf style
+      Please enter the number of your choice:');
+      subtypeName = ["Shrimp", "Veggie", "Elf style"];
+      break;
+      case "Salad":
+        subtype = prompt ('Select the type of pizza you want:
+        1 - Shrimp
+        2 - Greek
+        3 - Tomato
+        Please enter the number of your choice:');
+        subtypeName = ["Shrimp", "Greek", "Tomato"];
+        break;
+        default:
+          alert("invalid choice.");
+          ProcessingInstruction.exit(1);
+}
 
 let confirmation = prompt ("Are you sure?")
 
