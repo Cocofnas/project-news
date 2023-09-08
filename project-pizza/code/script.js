f0d89725a13d84c88e11cb5d99988977230c119e
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-09-08 12:58:33 +0200

    -m

--- project-pizza/code/script.js
+++ project-pizza/code/script.js
@@ -1,4 +1,4 @@
-// Start here
+141621// Start here
 
 // Step 1 - Welcome and introduction
 // Your code goes here
@@ -14,7 +14,7 @@ alert (
 
 // Step 2 - Food choice
 // Your code goes here
-const Food = prompt ("What's your craving today: 1 - Pizza  2 - Pasta  3- Salad Please enter the number of your choice!")
+let Food = prompt ("What's your craving today: 1 - Pizza  2 - Pasta  3- Salad Please enter the number of your choice!")
 
 let selectedFood ="";
 
@@ -44,7 +44,7 @@ alert (
 // Step 3 - Subtype choice
 // Your code goes here Ask the user to inser subtype based on choice of food.
 let subtype  = "";
-let subtypeName = ""; //Store name for the subtypes pizza, pasta and salad
+let subtypeName = []; //Store name for the subtypes pizza, pasta and salad
 
 switch (selectedFood) {
   case "Pizza":
@@ -52,50 +52,28 @@ switch (selectedFood) {
     subtypeName = ["Margherita", "Veggie", "Nutella"];
     break;
     case "Pasta":
-      subtype = prompt ('Select the type of pizza you want: 1 - Shrimp 2 - Veggie 3 - Elf style Please enter the number of your choice:');
+      subtype = prompt ('Select the type of pasta you want: 1 - Shrimppasta 2 - Veggiepasta 3 - Elf style pasta Please enter the number of your choice:');
       subtypeName = ["Shrimp", "Veggie", "Elf style"];
       break;
       case "Salad":
-        subtype = prompt ('Select the type of pizza you want: 1 - Shrimp 2 - Greek 3 - Tomato Please enter the number of your choice:');
+        subtype = prompt ('Select the type of salad you want: 1 - Shrimpsalad 2 - Greeksalad 3 - Tomatosalad Please enter the number of your choice:');
         subtypeName = ["Shrimp", "Greek", "Tomato"];
         break;
 }
-// To Technigo, I have tried out different codes now, and I now
 
-if (selectedFood === "Pizza")
-{
-  alert(
-  `You chose ${subtypeName[0]} as your meal`);
-  (
-    `You chose ${subtypeName[1]} as your meal`);
-    (
-      `You chose ${subtypeName[2]} as your meal`);
 
-  
-
-  } else if (selectedFood === "Pasta") {
-  alert(
-    `You chose ${subtypeName[0]} as your meal`);
-    (
-      `You chose ${subtypeName[1]} as your meal`);
-      (
-        `You chose ${subtypeName[2]} as your meal`);
+if (selectedFood === "Pizza") {
+  alert(`You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
+} else if (selectedFood === "Pasta") {
+  alert(`You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
+} else if (selectedFood === "Salad") {
+  alert(`You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
 }
-
-else if (selectedFood === "Salad") {
-alert(
-  `You chose ${subtypeName[0]} as your meal`);
-  (
-    `You chose ${subtypeName[1]} as your meal`);
-    (
-      `You chose ${subtypeName[2]} as your meal`);
-}
-
 // Step 4 - Age
 // Your code goes here
 
-let age = prompt ("Type your age in numbers")
-if (age>=18) {
+let age = prompt ("Type your age in numbers to help med figure out if you want a kid size or an adult size portion")
+if (age>=19) {
   alert (`The price is 150 SEK`);
 }
 else if (age<=18) {
@@ -109,17 +87,17 @@ else if (age<=18) {
 if (selectedFood === "Pizza")
 {
   alert(
-  `You chose ${selectedFood} and ${subtypeName} as your meal.`);
+  `You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
   
 
   } else if (selectedFood === "Pasta") {
   alert(
-    `You chose ${selectedFood} and ${subtypeName} as your meal.`);
+    `You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
 }
 
 else if (selectedFood === "Salad") {
 alert(
-  `You chose ${selectedFood} and ${subtypeName} as your meal.`);
+  `You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
 }
 
 let order = prompt ("Do you want to place an order? Yes or no")
